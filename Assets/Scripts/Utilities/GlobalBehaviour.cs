929ca777161ff45ecfaf9f5a40a243803b30403c
xuyanDavid11300 <73153499+xuyanDavid11300@users.noreply.github.com>
2022-09-24 17:13:41 +0100

    Delete Assets directory

--- Assets/Scripts/Utilities/GlobalBehaviour.cs
+++
@@ -1,9 +0,0 @@
-// inherit singleton features plus persistency across scenes.
-public class GlobalBehaviour<Persistent> : SingleBehaviour<Persistent> where Persistent : SingleBehaviour<Persistent>
-{
-    protected override void Awake()
-    {
-        base.Awake();
-        DontDestroyOnLoad(this);
-    }
-}
